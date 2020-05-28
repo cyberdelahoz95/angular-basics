9220d13af751c3552e112c450e1f05a4eaa9cee3
Henri de la Hoz <cyberdelahoz95@gmail.com>
2020-05-27 22:06:53 -0500

    styles lesson

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -14,6 +14,7 @@ import { Product } from '../models/product.model';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent implements OnChanges, OnInit, OnDestroy {
   @Input() product: Product;
