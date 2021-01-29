322541e800f38588162a4008344d40590172b905
Henry <cyberdelahoz95@gmail.com>
2021-01-28 19:52:02 -0500

    create product implemented

--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { ProductsService } from './../../../core/services/products/products.service';
 
 @Component({
   selector: 'app-form-product',
@@ -8,11 +11,25 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class FormProductComponent implements OnInit {
   form: FormGroup;
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private productService: ProductsService,
+    private router: Router
+  ) {
     this.buildForm();
   }
 
   ngOnInit(): void {}
+
+  saveProduct(event: Event) {
+    event.preventDefault();
+    if (this.form.valid) {
+      const product = this.form.value;
+      this.productService.createProduct(product).subscribe((createdProduct) => {
+        this.router.navigate(['/admin/products']);
+      });
+    }
+  }
 
   private buildForm() {
     this.form = this.fb.group({
